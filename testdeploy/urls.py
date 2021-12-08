46f79754c4ea1a6e6cb5bd4b398bf6348e0a2fd2
surendhar-1706 <surendhar2k18@gmail.com>
2021-12-08 09:36:34 +0530

    static

--- testdeploy/urls.py
+++ testdeploy/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_swagger.views import get_swagger_view
 from django.conf.urls import url
 from django.conf import settings
@@ -24,9 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('crud.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    url(r'^$', schema_view),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT})
+    re_path(r'^$', schema_view),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root':       settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT})
 ]
